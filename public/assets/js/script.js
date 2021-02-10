ffcac8812b0feff8309654ac9722bbf4c5339dbf
watercolorhorse <29869241+llcook@users.noreply.github.com>
2021-02-09 22:21:49 -0700

    post newContact on click now functional

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,18 @@
 // add contact functionality
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+    var newContact = {
+        firstName: $("#firstName").val().trim(),
+        lastName: $("#lastName").val().trim(),
+        contactType: $("#contactType").val(),
+        phoneNumber: $("#phoneNumber").val().trim(),
+        emailAddress: $("#emailAddress").val().trim()
+    }
 
+    $.post("/api/contacts", newContact).then(function(response) {
+        location.href = "/";
+    });
+})
 // filter contact by type functionality
 
 // delete contact functionality
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -20,9 +20,17 @@ module.exports = function(app) {
     // this route should find all contacts of a particular type (Personal or Business) and render them 
     // using the Handlebars 'contacts' template, sorted ascending by lastName, then by firstName
     app.get("/:type", function(req, res) {
-
+        var type = req.params.type[0].toUpperCase() + req.params.type.substring(1);
+        db.Contact.findAll({
+            where: {
+                contactType: req.params.type
+            },
+            order: [["lastName", "ASC"], ["firstName", "ASC"]]
+        }).then(function(contacts) {
+            console.log(contacts);
+            res.render("contacts", { contacts, type, name: "Lanie" })
+        });
+    });
         // BONUS: pass the contact type through to the handlebars template... be sure
         // to make the contact type title-cased!
-
-    });
 }
